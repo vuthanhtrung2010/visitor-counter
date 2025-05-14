66dbfe1a4a3d193f027a1b9a2c84d598a4d4f38d
Vũ Thành Trung <vuthanhtrungsuper@gmail.com>
2025-05-14 17:04:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.get('/', (req, res) => {
-  res.send('Server is running');
+  res.send('Server is running, https://github.com/vuthanhtrung2010/visitor-counter/');
 });
 
 app.get('/:username/count.svg', async (req, res) => {
@@ -37,7 +37,6 @@ app.get('/:username/count.svg', async (req, res) => {
     `;
   }).join('');
 
-  // 5) assemble final SVG
   const svg = `
     <svg xmlns="http://www.w3.org/2000/svg"
          width="${totalW}" height="${totalH}">
